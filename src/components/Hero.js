fc9317f0e9c520261aa9d259ea9ecd3dcd7ccab0
shubham <shubh.shubham21@gmail.com>
2022-05-23 19:20:09 +0530

    update scolling with react-scroll and added navbar active function

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import './Hero.css'
+import React from 'react';
+import './Hero.css';
 
 const Hero = () => {
-    return (
-        <div className='hero'>
-            <div className='content'>
-                <p>Call us</p>
-                <p>1-800-123-4567</p>
-                <p>Because Money</p>
-                <p>Doesn't come with instructions</p>
-                <button href='/' className='button'>Free Consultation</button>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className='hero' id='hero'>
+			<div className='content'>
+				<p>Call us</p>
+				<p>1-800-123-4567</p>
+				<p>Because Money</p>
+				<p>Doesn't come with instructions</p>
+				<button href='/' className='button'>
+					Free Consultation
+				</button>
+			</div>
+		</div>
+	);
+};
 
-export default Hero
+export default Hero;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link } from 'react-scroll';
 import logo from './images/logo.png';
 
 import './Navbar.css';
@@ -28,9 +29,9 @@ const Navbar = () => {
 	return (
 		<div className={color ? 'header header-bg' : 'header'}>
 			<nav className='navbar'>
-				<a href='/' className='logo'>
+				<Link href='/' className='logo'>
 					<img src={logo} alt='logo' />
-				</a>
+				</Link>
 				<div className='hamburger' onClick={handleClick}>
 					{click ? (
 						<FaTimes size={30} style={{ color: '#ffffff' }} />
@@ -40,24 +41,52 @@ const Navbar = () => {
 				</div>
 				<ul className={click ? 'nav-menu active' : 'nav-menu'}>
 					<li className='nav-item'>
-						<a href='/' onClick={closeMenu}>
+						<Link
+							to='hero'
+							spy={true}
+							smooth={true}
+							offset={-100}
+							duration={500}
+							onClick={closeMenu}
+						>
 							Home
-						</a>
+						</Link>
 					</li>
 					<li className='nav-item'>
-						<a href='#about' onClick={closeMenu}>
+						<Link
+							to='about'
+							spy={true}
+							smooth={true}
+							offset={-100}
+							duration={500}
+							onClick={closeMenu}
+						>
 							About
-						</a>
+						</Link>
 					</li>
 					<li className='nav-item'>
-						<a href='#testimonials' onClick={closeMenu}>
+						<Link
+							to='testimonials'
+							spy={true}
+							smooth={true}
+							offset={-100}
+							duration={500}
+							onClick={closeMenu}
+						>
 							Testimonials
-						</a>
+						</Link>
 					</li>
 					<li className='nav-item'>
-						<a href='#demo' onClick={closeMenu}>
+						<Link
+							to='demo'
+							spy={true}
+							smooth={true}
+							offset={-150}
+							duration={500}
+							onClick={closeMenu}
+						>
 							Demo
-						</a>
+						</Link>
 					</li>
 				</ul>
 			</nav>
