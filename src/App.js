0797e9c9a23e6907a7a4d1db568118d6b60c2c63
shubham <shubh.shubham21@gmail.com>
2022-05-23 17:46:50 +0530

    complete

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Testimonials from './components/Testimonials'
+import Demo from './components/Demo'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Testimonials />
+      <Demo />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
