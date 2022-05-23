13cea1e1728ca5fb482f26cd69c03a2a0429e3a7
shubham <shubh.shubham21@gmail.com>
2022-05-23 18:49:55 +0530

    update navbar change color on scrolling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,68 @@
-import React, { useState } from 'react'
-import { FaBars, FaTimes } from 'react-icons/fa'
-import logo from './images/logo.png'
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import logo from './images/logo.png';
 
-import './Navbar.css'
+import './Navbar.css';
 
 const Navbar = () => {
+	// setting mobile nav
+	const [click, setClick] = useState(false);
+	const handleClick = () => setClick(!click);
 
-    const [click, setClick] = useState(false)
-    const handleClick = () => setClick(!click)
+	// change navbar color when scrolling
+	const [color, setColor] = useState(false);
 
-    const closeMenu = () => setClick(false)
+	const changeColor = () => {
+		if (window.scrollY >= 90) {
+			setColor(true);
+		} else {
+			setColor(false);
+		}
+	};
 
-    return (
-        <div className='header'>
-            <nav className='navbar'>
-                <a href='/' className='logo'>
-                    <img src={logo} alt='logo' />
-                </a>
-                <div className='hamburger' onClick={handleClick}>
-                    {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
-                        : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
+	window.addEventListener('scroll', changeColor);
 
-                </div>
-                <ul className={click ? "nav-menu active" : "nav-menu"}>
-                    <li className='nav-item'>
-                        <a href='/' onClick={closeMenu}>Home</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a href='#about' onClick={closeMenu}>About</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a href='#testimonials' onClick={closeMenu}>Testimonials</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a href='#demo' onClick={closeMenu}>Demo</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    )
-}
+	// close menu on click
+	const closeMenu = () => setClick(false);
 
-export default Navbar
+	return (
+		<div className={color ? 'header header-bg' : 'header'}>
+			<nav className='navbar'>
+				<a href='/' className='logo'>
+					<img src={logo} alt='logo' />
+				</a>
+				<div className='hamburger' onClick={handleClick}>
+					{click ? (
+						<FaTimes size={30} style={{ color: '#ffffff' }} />
+					) : (
+						<FaBars size={30} style={{ color: '#ffffff' }} />
+					)}
+				</div>
+				<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+					<li className='nav-item'>
+						<a href='/' onClick={closeMenu}>
+							Home
+						</a>
+					</li>
+					<li className='nav-item'>
+						<a href='#about' onClick={closeMenu}>
+							About
+						</a>
+					</li>
+					<li className='nav-item'>
+						<a href='#testimonials' onClick={closeMenu}>
+							Testimonials
+						</a>
+					</li>
+					<li className='nav-item'>
+						<a href='#demo' onClick={closeMenu}>
+							Demo
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
+};
+
+export default Navbar;
